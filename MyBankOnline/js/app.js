7941f6b886e47f047c62bce88530f0a8e432b866
3mychanneljack-code <3mychanneljack@gmail.com>
2026-03-07 20:46:51 -0600

    Update app.js

--- MyBankOnline/js/app.js
+++ MyBankOnline/js/app.js
@@ -1,19 +1,37 @@
 const API = "https://mybankonline.onrender.com";
 
-function login(){
+function register(){
 
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
+const username=document.getElementById("username").value;
+const password=document.getElementById("password").value;
 
-fetch(API + "/login",{
+fetch(API+"/register",{
 method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
+headers:{"Content-Type":"application/json"},
 body:JSON.stringify({username,password})
 })
-.then(res => res.json())
-.then(data => {
+.then(r=>r.json())
+.then(data=>{
+if(data.success){
+alert("Account created");
+window.location="login.html";
+}
+});
+
+}
+
+function login(){
+
+const username=document.getElementById("username").value;
+const password=document.getElementById("password").value;
+
+fetch(API+"/login",{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({username,password})
+})
+.then(r=>r.json())
+.then(data=>{
 
 if(data.success){
 
@@ -21,10 +39,6 @@ localStorage.setItem("token",data.token);
 localStorage.setItem("balance",data.balance);
 
 window.location="dashboard.html";
-
-}else{
-
-alert("Login failed");
 
 }
 
@@ -34,37 +48,31 @@ alert("Login failed");
 
 function transfer(){
 
-const to = document.getElementById("toUser").value;
-const amount = document.getElementById("amount").value;
+const to=document.getElementById("toUser").value;
+const amount=document.getElementById("amount").value;
 
-fetch(API + "/transfer",{
+fetch(API+"/transfer",{
 method:"POST",
 headers:{
 "Content-Type":"application/json",
 "Authorization":localStorage.getItem("token")
 },
 body:JSON.stringify({to,amount})
-})
-.then(res => res.json())
-.then(data => {
-
-if(data.success){
-alert("Transfer complete");
-}else{
-alert("Transfer failed");
-}
-
 });
 
 }
 
-function loadUsers(){
+function loadTransactions(){
 
-fetch(API + "/admin/users")
-.then(res => res.json())
-.then(data => {
+fetch(API+"/transactions",{
+headers:{
+"Authorization":localStorage.getItem("token")
+}
+})
+.then(r=>r.json())
+.then(data=>{
 
-document.getElementById("users").innerHTML =
+document.getElementById("transactions").innerHTML=
 "<pre>"+JSON.stringify(data,null,2)+"</pre>";
 
 });
